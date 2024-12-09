4d5eac1b7db2dc7b58ddc4b99ef9fd22898d8368
napodhva7e <pasha.laba@mail.ru>
2024-12-09 20:53:32 +0300

    add PopUp window

--- script/script.js
+++ script/script.js
@@ -83,5 +83,70 @@ window.onclick = function (event) {
   }
 };
 
-// FAQ 
+// popUp
+const scrollController = {
+  scrollTPosition: 0,
+  disabledScroll() {
+    scrollController.scrollTPosition = window.scrollY;
+    document.body.style.cssText = `
+    overflow: hidden;
+    position: fixed;
+    top: -${scrollController.scrollTPosition}px;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    padding-right: ${window.innerWidth - document.body.offsetWidth}px;
+    `;
+  },
+  enabledScroll() {
+    document.body.style.cssText = ``;
+    window.scroll({ top: scrollController.scrollTPosition });
+  },
+};
 
+const modalController = ({ modal, btnOpen, btnClose, time = 300 }) => {
+  const buttonElem = document.querySelector(btnOpen);
+  const modalElem = document.querySelector(modal);
+
+  modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity ${time}ms ease-in-out;
+  `;
+
+  const closeModal = (event) => {
+    const target = event.target;
+
+    if (
+      target === modalElem ||
+      (btnClose && target.closest(btnClose)) ||
+      event.code === "Escape"
+    ) {
+      modalElem.style.opacity = 0;
+
+      setTimeout(() => {
+        modalElem.style.visibility = "hidden";
+        scrollController.enabledScroll();
+      }, time);
+      window.removeEventListener("keydown", closeModal);
+    }
+  };
+
+  const openModal = () => {
+    modalElem.style.visibility = "visible";
+    modalElem.style.opacity = 1;
+    window.addEventListener("keydown", closeModal);
+    scrollController.disabledScroll();
+  };
+
+  buttonElem.addEventListener("click", openModal);
+
+  modalElem.addEventListener("click", closeModal);
+};
+
+modalController({
+  modal: ".modal",
+  btnOpen: ".section__button",
+  btnClose: ".modal__close",
+});
